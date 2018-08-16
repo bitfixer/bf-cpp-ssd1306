af9d54834edb3364bf79dca894a76e955b8ca18e
Michael Hill <gubbish@Michaels-MacBook-Pro.local>
2018-08-15 23:09:38 -0700

    add ssd-gfx

---
+++ bf-ssd-gfx/ssd-gfx.h
@@ -0,0 +1,26 @@
+#ifndef __ssd_gfx_h__
+#define __ssd_gfx_h__
+
+#include "gfx.h"
+#include "Adafruit_GFX.h"
+
+class SSDGFX
+{
+public:
+	SSDGFX(int16_t w, int16_t h, GFX* gfx)
+	: Adafruit_GFX(w, h)
+	, _gfx(gfx)
+	{
+
+	}
+
+	void drawPixel(int16_t x, int16_t y, uint16_t color)
+    {
+        _gfx->setpixel(x, y, 255);
+    }
+
+private:
+	GFX* _gfx;
+}
+
+#endif
